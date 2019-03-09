e5714dcf2f18e53bf0862c86aaf01d9dbdc7015f
Jeff Levine <jlevine_84@yahoo.com>
2019-03-09 13:34:25 -0500

    update

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const htmlRoute = require("./routing/htmlRouting")
 const apiRoute = require("./routing/apiRouting")
 
 const server = express();
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
 htmlRoute(server, path)
 apiRoute(server, express, reservations)
